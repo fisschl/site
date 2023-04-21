55b78700511efdce02dc889c3bc11c04496f3d7a
Fisschl <fisschl@163.com>
2023-04-21 15:29:58 +0800

    新增：element plus

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
+<script setup lang="ts">
+import { IconApps } from "@tabler/icons-vue";
+</script>
+
 <template>
   <nav
-    class="sticky top-3 z-10 m-3 rounded-md bg-white/20 backdrop-blur dark:bg-black/20"
+    class="sticky top-3 z-10 m-3 flex items-center overflow-hidden rounded-lg bg-white/20 px-4 py-3 backdrop-blur dark:bg-black/20"
   >
-    <h1 class="text-lg font-semibold">大道之行也 天下为公</h1>
+    <h1 class="flex-1 text-center text-lg">大道之行也 天下为公</h1>
+    <ElButton text round class="mr-2">
+      <IconApps :size="22" />
+    </ElButton>
   </nav>
   <main>
     <slot />
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useElementSize } from "@vueuse/core";
+import type { ElCarousel } from "element-plus";
+
+const carousel = ref<InstanceType<typeof ElCarousel>>();
+const { width } = useElementSize(() => carousel.value?.$el);
+
+const controlData = [
+  {
+    url: "https://tenapi.cn/v2/bing",
+  },
+  {
+    url: "https://tenapi.cn/v2/acg",
+  },
+];
+</script>
 
 <template>
   <div>
-    <ElButton> Search </ElButton>
+    <ElCarousel ref="carousel" :height="width * (9 / 16) + 'px'">
+      <ElCarouselItem v-for="item in controlData" :key="item.url">
+        <img :src="item.url" :alt="item.url" />
+      </ElCarouselItem>
+    </ElCarousel>
   </div>
 </template>
