db68b49818c681fa5b00f7f17e6cd32f3e37604a
WangZhenghai <fisschl@163.com>
2023-04-24 10:44:53 +0800

    新增：今日诗词

---
+++ components/JinRiShiCi.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const id = "#jinrishici-script";
+onMounted(() => {
+  const ele = document.querySelector(id);
+  if (ele) ele.remove();
+  const script = document.createElement("script");
+  script.src = "https://sdk.jinrishici.com/v2/browser/jinrishici.js";
+  script.id = id;
+  document.body.appendChild(script);
+});
+</script>
+
+<template>
+  <p class="jinrishici-sentence mb-4 mt-2 text-center"></p>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
+  <JinRiShiCi />
   <HeadCarousel />
   <TopSearchList />
 </template>
