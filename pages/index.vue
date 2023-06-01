8074128a8d698ad3d64d826e278598d435ac5dc1
WangZhenghai <fisschl@163.com>
2023-06-01 09:18:34 +0800

    feat: 热搜移动到新页面

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IconArrowBigRight } from "@tabler/icons-vue";
+import type { TopSearchResponse } from "./topsearch.vue";
+import { NuxtLink } from "#components";
+
+const { data } = useAsyncData(() =>
+  request<TopSearchResponse>("/topsearch", {
+    query: { page: 1, size: 10 },
+  })
+);
+</script>
 
 <template>
   <main>
@@ -10,6 +20,28 @@
       />
     </section>
     <JinRiShiCi class="my-10" />
-    <TopSearchList />
+    <section class="mx-auto my-10 max-w-xl px-4">
+      <ElCard v-if="data" class="">
+        <ul class="ml-6 list-disc space-y-3 text-slate-400 marker:text-sky-400">
+          <li v-for="item in data.list" :key="item.id">
+            <a
+              :href="item.url"
+              target="_blank"
+              class="text-sm hover:text-indigo-400 hover:underline"
+            >
+              {{ item.title }}
+            </a>
+          </li>
+        </ul>
+        <div class="flex justify-center">
+          <ElButton text bg to="/topsearch" :tag="NuxtLink">
+            热搜分析
+            <ElIcon :size="20" class="ml-2">
+              <IconArrowBigRight />
+            </ElIcon>
+          </ElButton>
+        </div>
+      </ElCard>
+    </section>
   </main>
 </template>
