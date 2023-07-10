a75cd5c4231d7f440ba2075b4d4c5b2fdb81f6dd
WangZhenghai <fisschl@163.com>
2023-07-10 16:34:29 +0800

    fix(热搜): 请求方式

--- pages/top-search.vue
+++ pages/top-search.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { omit, pick } from "lodash-es";
+import { debounce, omit, pick } from "lodash-es";
 import type { Page } from "utils/page";
 
 interface TopSearchItem {
@@ -20,7 +20,7 @@ const { data: topSearchType } = useAsyncData(() =>
 
 const page = reactive<Page>({
   page: 1,
-  size: 60,
+  size: 20,
 });
 
 const formData = reactive<
@@ -30,8 +30,9 @@ const formData = reactive<
 >({});
 
 const data = ref<TopSearchItem[]>();
-
-const fetchData = () => {
+const loading = ref(false);
+const fetchData = debounce(() => {
+  loading.value = true;
   const dateRange = formData.dateRange || [];
   return request<TopSearchResponse>("/top-search", {
     query: {
@@ -40,23 +41,20 @@ const fetchData = () => {
       start_time: dateRange[0] || undefined,
       end_time: dateRange[1] || undefined,
     },
-  }).then((res) => {
-    page.total = res.total;
-    data.value = res.list.map((item) => {
-      item.update_time = formatShowTime(item.update_time);
-      return item;
+  })
+    .then((res) => {
+      page.total = res.total;
+      data.value = res.list.map((item) => {
+        item.update_time = formatShowTime(item.update_time);
+        return item;
+      });
+    })
+    .finally(() => {
+      loading.value = false;
     });
-  });
-};
-
-watchDebounced(() => [formData, page], fetchData, {
-  debounce: 500,
-  deep: true,
-  immediate: true,
-});
-watch(formData, () => {
-  page.page = 1;
-});
+}, 200);
+fetchData();
+watchDeep(formData, () => (page.page = 1));
 </script>
 
 <template>
@@ -69,12 +67,14 @@ watch(formData, () => {
         placeholder="标题"
         clearable
         style="max-width: 20rem"
+        @change="fetchData"
       />
       <ElSelect
         v-model="formData.type"
         placeholder="类型"
         clearable
         style="max-width: 10rem"
+        @change="fetchData"
       >
         <ElOption
           v-for="item in topSearchType"
@@ -90,9 +90,14 @@ watch(formData, () => {
         style="max-width: 17rem"
         start-placeholder="开始时间"
         end-placeholder="结束时间"
+        @change="fetchData"
       />
     </form>
-    <ElTable class="mx-4 !w-auto" :data="data || []">
+    <ElTable
+      v-loading="loading"
+      class="mx-4 min-h-2xl !w-auto"
+      :data="data || []"
+    >
       <ElTableColumn label="标题" prop="title">
         <template #default="{ row }">
           <ElLink :href="row.url" target="_blank">
@@ -107,7 +112,7 @@ watch(formData, () => {
           </ElTag>
         </template>
       </ElTableColumn>
-      <ElTableColumn label="更新时间" width="140" prop="update_time">
+      <ElTableColumn label="更新时间" width="160" prop="update_time">
         <template #default="{ row }">
           <ElTag size="small" type="success">
             {{ row.update_time }}
@@ -121,8 +126,10 @@ watch(formData, () => {
         v-model:page-size="page.size"
         class="mx-auto"
         :total="page.total || 0"
-        :page-sizes="[40, 80, 120, 200]"
+        :page-sizes="[20, 60, 100]"
         layout="total, sizes, prev, pager, next, jumper"
+        @update:current-page="fetchData"
+        @update:page-size="fetchData"
       />
     </div>
   </main>
